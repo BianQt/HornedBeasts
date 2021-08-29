65ff4956d7d54f55aa79b1b89acb58b7d5de412b
BianQt <bayanqutshan@gmail.com>
2021-08-29 20:14:29 +0300

    lab01-complete

---
+++ horned-beasts/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+    <h1>Horned Animals</h1>
+    )
+  }
+}
+
+export default Header;
