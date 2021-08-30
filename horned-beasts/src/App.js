e28ad24c889fd18d400d329bb6e0656860a1d82c
BianQt <bayanqutshan@gmail.com>
2021-08-30 10:50:53 +0300

    add eslintc

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -7,15 +7,15 @@ function App() {
   return (
     <div className="App">
       <header>
-      <Header />
+        <Header />
       </header>
       <main>
-      <Main />
+        <Main />
       </main>
       <footer>
-      <Footer />
+        <Footer />
       </footer>
-      
+
     </div>
   );
 }
--- horned-beasts/src/components/Header.js
+++ horned-beasts/src/components/Header.js
@@ -3,9 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     console.log(this.props);
-    return (
-    <h1>Horned Animals</h1>
-    )
+    return <h1>Horned Animals</h1>;
   }
 }
 
